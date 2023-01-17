c8dd7280f2ecf11a975a34d69282a338df00d45e
Bo Lopker <lopkerk@gmail.com>
2023-01-16 19:23:23 -0800

    Cleanup

--- lib/flutter_btleplug.dart
+++
@@ -1,9 +0,0 @@
-export 'ffi.dart';
-
-import 'flutter_btleplug_platform_interface.dart';
-
-class Btleplug {
-  Future<String?> getPlatformVersion() {
-    return BtleplugPlatform.instance.getPlatformVersion();
-  }
-}
