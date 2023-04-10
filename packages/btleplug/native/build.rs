a0dc272f7d419db9bca1bdf3e23ecc0518453a56
Daniel Hahne <d@niel.re>
2023-04-10 21:24:56 +0200

    chore: Remove specific config for building on Arch Linux in build.rs.

--- packages/btleplug/native/build.rs
+++ packages/btleplug/native/build.rs
@@ -1,5 +1,3 @@
-use std::process::Command;
-
 use lib_flutter_rust_bridge_codegen::{
     config_parse, frb_codegen, get_symbols_if_no_duplicates, RawOpts,
 };
@@ -14,19 +12,6 @@ fn main() {
     // Tell Cargo that if the input Rust code changes, rerun this build script
     println!("cargo:rerun-if-changed={}", RUST_INPUT);
 
-    let llvm_version = Command::new("llvm-config")
-        .arg("--version")
-        .output()
-        .unwrap()
-        .stdout;
-    let llvm_version = std::str::from_utf8(&llvm_version[..llvm_version.len() - 1]).unwrap();
-    let llvm_libdir = Command::new("llvm-config")
-        .arg("--libdir")
-        .output()
-        .unwrap()
-        .stdout;
-    let llvm_libdir = std::str::from_utf8(&llvm_libdir[..llvm_libdir.len() - 1]).unwrap();
-
     // Options for frb_codegen
     let raw_opts = RawOpts {
         rust_input: vec![RUST_INPUT.to_string()],
@@ -35,7 +20,6 @@ fn main() {
         extra_c_output_path: Some(vec![MACOS_C_OUTPUT.to_string()]),
         inline_rust: true,
         wasm: true,
-        llvm_compiler_opts: Some(format!("-I{llvm_libdir}/clang/{llvm_version}/include")),
         ..Default::default()
     };
 
